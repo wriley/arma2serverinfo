4bff9ddcaf1bd6024e1d0b8bb60e6f89d18c38cc
William Riley <william@therileys.freetcp.com>
2014-07-08 17:10:59 -0500

    added port and server tags

--- main.c
+++ main.c
@@ -11,11 +11,12 @@ int timedout = 0;
 #define TIMEOUT_SECS    5
 void CatchAlarm(int unused);
 char *getString(char *rbuf, int *sPtr);
+void hexDump (char *desc, void *addr, int len);
 
 int main(int argc, char* argv[]) {
     int sockfd,n,i,j,sPtr;
     struct sockaddr_in servaddr,cliaddr;
-    char rbuf[1500];
+    unsigned char rbuf[1500], edf, s1, s2, i1, i2, i3, i4;
     char A2S_INFO[25] = {0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00};
     struct sigaction myAction;
     char *info;
@@ -78,6 +79,8 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    //hexDump("rbuf", &rbuf, n);
+
     // https://developer.valvesoftware.com/wiki/Server_queries#Response_Format
     //Header
     printf("HEADER: 0x%x\n", rbuf[4]);
@@ -107,9 +110,9 @@ int main(int argc, char* argv[]) {
     free(info);
 
     // ID
-    unsigned char id1 = rbuf[sPtr++];
-    unsigned char id2 = rbuf[sPtr++];
-    uint16_t id = id2<<8 | id1;
+    s1 = rbuf[sPtr++];
+    s2 = rbuf[sPtr++];
+    uint16_t id = s2<<8 | s1;
     printf("ID: %d\n", id);
 
     // Players
@@ -132,6 +135,37 @@ int main(int argc, char* argv[]) {
 
     // VAC
     printf("VAC: %d\n", rbuf[sPtr++]);
+
+    // Version
+    info = getString(rbuf, &sPtr);
+    printf("VERSION: %s\n", info);
+    free(info);
+
+    // Check for extra data
+    if(n > sPtr) {
+        // EDF
+        edf = rbuf[sPtr++];
+
+        // PORT
+        if(edf & 0x80) {
+            s1 = rbuf[sPtr++];
+            s2 = rbuf[sPtr++];
+            uint16_t port = s1 | s2<<8;
+            printf("PORT: %d\n", port);
+        }
+
+        // STEAMID
+        if(edf & 0x10) {
+            sPtr += 8;
+        }
+
+        // SERVER TAGS
+        if(edf & 0x20) {
+            info = getString(rbuf, &sPtr);
+            printf("SERVER TAGS: %s\n", info);
+            free(info);
+        }
+    }
 
     return 0;
 }
@@ -164,4 +198,48 @@ char *getString(char *rbuf, int *sPtr) {
         line[index++] = ch;
     }
     return line;
+}
+
+// from http://stackoverflow.com/questions/7775991/how-to-get-hexdump-of-a-structure-data
+void hexDump (char *desc, void *addr, int len) {
+    int i;
+    unsigned char buff[17];
+    unsigned char *pc = (unsigned char*)addr;
+
+    // Output description if given.
+    if (desc != NULL)
+        printf ("%s:\n", desc);
+
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf ("  %s\n", buff);
+
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        printf ("   ");
+        i++;
+    }
+
+    // And print the final ASCII bit.
+    printf ("  %s\n", buff);
 }
